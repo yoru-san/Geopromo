a12907ed72ee2d0f0bc40c5a2f74976c717b6b2c
yoru-san <cecel31@free.fr>
2017-03-08 22:43:46 +0100

    début palette

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,8 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'ngMaterial'
   ])
   .config(function ($routeProvider) {
     $routeProvider
@@ -27,4 +28,30 @@ angular
       .otherwise({
         redirectTo: '/'
       });
+  })
+  .config(function ($mdThemingProvider) {
+    $mdThemingProvider.definePalette('chiasse', {
+       '50': '#FAFAFA',
+       '100': '#F5F5F5',
+       '200': '#EEEEEE',
+       '300': '#E0E0E0',
+       '400': '#BDBDBD',
+       '500': '#9E9E9E',
+       '600': '#757575',
+       '700': '#616161',
+       '800': '#424242',
+       '900': '#212121',
+       'A100': '#FAFAFA',
+       'A200': '#FAFAFA',
+       'A400': '#FAFAFA',
+       'A700': '#FAFAFA',
+       'contrastDefaultColor': 'light',
+       'contrastDarkColors': ['50', '100', '200', '300', 'A100'],
+       'contrastLightColors': undefined
+    });
+
+     $mdThemingProvider.theme('default')
+        .primaryPalette('teal')
+        .accentPalette('green')
+        .warnPalette('purple');
   });
